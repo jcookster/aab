8ab796327a3a95a7448905d67289a9a1e13fcaad
Jonathan Cook <jon@jcook.org>
2011-07-13 12:22:31 -0700

    nav done

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
 /* Author: 
 
 */
+$(document).ready(function(){
+  
+		var existingClasses;
+		
+		$("div#navHome").mouseover(function(){
+			existingClasses = $(this).attr("class");
+			$("div#navHome").removeClass().addClass("selectedNavigationItem navigationBlock");
+		}).mouseout(function(){
+			$("div#navHome").removeClass().addClass(existingClasses);		
+		});
+		
 
+		$("div#navInventory").mouseover(function(){
+			existingClasses = $(this).attr("class");
+			$("div#navInventory").removeClass().addClass("selectedNavigationItem navigationBlock");
+			
+		}).mouseout(function(){
+			$("div#navInventory").removeClass().addClass(existingClasses);		
+		});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		$("div#navAbout").mouseover(function(){
+			existingClasses = $(this).attr("class");
+			$("div#navAbout").removeClass().addClass("selectedNavigationItem navigationBlock");
+		}).mouseout(function(){
+			$("div#navAbout").removeClass().addClass(existingClasses);		
+		});
+		
+		$("div#navDirections").mouseover(function(){
+			existingClasses = $(this).attr("class");  
+			$("div#navDirections").removeClass().addClass("navigationBlock selectedNavigationItem ");
+		}).mouseout(function(){
+			$("div#navDirections").removeClass().addClass(existingClasses);		
+		});
+		
+		
+  });
